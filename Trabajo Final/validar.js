13a838a4c76b906597f59994bcd370f57bcd6d5e
Angiie25031997 <84481784+Angiie25031997@users.noreply.github.com>
2022-02-07 20:42:48 -0300

    Update validar.js

--- Trabajo Final/validar.js
+++ Trabajo Final/validar.js
@@ -1,35 +1,4 @@
-/*
-function validar(){
-	let id = (id) => document.getElementById(id);
 
-	let classes = (classes) => document.getElementsByClassName(classes);
-
-	let nombre = id("nombre"),
-		apellido = id("apellido"),
-		dni = id("dni"),
-	  email = id("email"),
-	  form = id("form"),
-	  errorMsg = classes("error");
-	
-	form.addEventListener("submit", (e) => {
-	  engine(nombre, 0, "Ingrese su nombre");
-	  engine(apellido, 1, "Ingrese su apellido");
-	  engine(dni, 2, "Ingrese su dni");
-	  engine(email, 3, "Ingrese su email");
-	  
-	});
-
-	let engine = (id, serial, message) => {
-	  if (id.value.trim() === "") {
-		errorMsg[serial].innerHTML = message;
-		id.style.border = "2px solid red";
-		return false;
-	  } else {
-		errorMsg[serial].innerHTML = "";
-		id.style.border = "0px solid transparent"; 
-	  } 
-	};
-}*/
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("form").addEventListener('submit', validarFormulario); 
